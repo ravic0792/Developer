a2169a6c494a476753b1107e9a1b08ceadcd9503
ravi <ravic0792@gmail.com>
2025-01-03 23:24:08 -0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express =require('express')
+const {PORT}=require('./config/serverConfig')
+const app=express()
+
+const prepareAndStartServer= ()=>{
+    app.listen(PORT,()=>{
+            console.log(`server started on PORT:${PORT}`);
+
+    })
+}
+
+prepareAndStartServer();
+
+
